05df56c534948eb917b67737531077eddb2e858b
JGprogramacion <jgprogramacion23@gmail.com>
2025-01-21 13:36:33 -0500

    Código numeros primos

--- numerosPrimos.cpp
+++ numerosPrimos.cpp
@@ -3,8 +3,22 @@ using namespace std;
 
 int main (){
     int numero;
-
+    int calculo=1;
+    int cont=0;
     cin>>numero;
 
-    if(numero)
+    for(int i=1; i<=numero;i++){
+        calculo = numero%i;
+        
+        if(calculo==0){
+            cont = cont+1;
+        }
+        cout<<"i "<<i<<" calculo "<<calculo<<endl<<"contador "<<cont<<endl;
+    }
+    if(cont>1 && cont <3){
+        cout<<"Es un numero primo";
+    }
+    else{
+        cout<<"No es un numero primo";
+    }
 }
